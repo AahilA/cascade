b5aaff73b80e551430da40cc1be67ae5f8588e49
AahilA <asa97@cornell.edu>
2021-02-10 13:34:03 -0500

    All Python LINQ objects made

--- src/service/python/cascadeLINQ.py
+++ src/service/python/cascadeLINQ.py
@@ -2,33 +2,103 @@
 import cascade_py
 
 class CascadeShardLinq:
-    def __init__(self, capi, subgroup_index, shard_index, _type, version=-2, ts=-1):
+    def __init__(self, capi, _type, subgroup_index, shard_index, version):
         self.capi = capi
         self.subgroup_index = subgroup_index
         self.shard_index = shard_index
         self.version = version
-        self.ts = ts
         self.type = _type
         self.gi_running = True
 
     def __iter__(self):
 
-        if self.version != -2:
-            store = self.capi.get_keylist(self.type, self.version, self.subgroup_index, self.shard_index)
-            print("GOT STORE", store)
-            self.key_list = store.get_result()
-            print("GOT RESULT", self.key_list)
-        else:
-            store = self.capi.get_keylist_by_time(self.type, self.ts, self.subgroup_index, self.shard_index)
-            self.key_list = store.get_result()
+        store = self.capi.get_keylist(self.type, self.version, self.subgroup_index, self.shard_index)
+        self.key_list = store.get_result()
         return self
 
     def __next__(self):
         if len(self.key_list) != 0:
             store = self.capi.get(self.type, str(self.key_list.pop(0)), self.version, self.subgroup_index, self.shard_index)
-            print("GOT STORE1", store)
-            value = store.get_result()
-            print("GOT RESULT1", value)
+            value = store.get_result().bytes()
             return value.decode('utf-8')
         else:
             raise StopIteration
+
+class CascadeShardLinqTime:
+    def __init__(self, capi, _type, subgroup_index, shard_index, ts):
+        self.capi = capi
+        self.subgroup_index = subgroup_index
+        self.shard_index = shard_index
+        self.ts = ts
+        self.type = _type
+        self.gi_running = True
+
+    def __iter__(self):
+    
+        store = self.capi.get_keylist_by_time(self.type, self.ts, self.subgroup_index, self.shard_index)
+        self.key_list = store.get_result()
+        return self
+
+    def __next__(self):
+        if len(self.key_list) != 0:
+            store = self.capi.get(self.type, str(self.key_list.pop(0)), self.version, self.subgroup_index, self.shard_index)
+            value = store.get_result().bytes()
+            return value.decode('utf-8')
+        else:
+            raise StopIteration
+
+class CascadeShardLinqVersion:
+    def __init__(self, capi, _type, subgroup_index, shard_index, key, version):
+        self.capi = capi
+        self.subgroup_index = subgroup_index
+        self.shard_index = shard_index
+        self.version = version
+        self.key = key
+        self.type = _type
+        self.over = False
+        self.gi_running = True
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.over:
+            store = self.capi.get(self.type, self.key, self.version, self.subgroup_index, self.shard_index)
+            obj = store.get_result()
+            version = obj.previous_version_by_key()
+            value = obj.bytes()
+            if version == 1:
+                self.over = True
+            return value.decode('utf-8')
+        else:
+            raise StopIteration
+
+class CascadeShardLinqSubgroup:
+    def __init__(self, capi, _type, subgroup_index, version):
+        self.capi = capi
+        self.subgroup_index = subgroup_index
+        self.version = version
+        self.shard_list = []
+        self.type = _type
+        self.ptr = 0
+        self.gi_running = True
+
+    def __iter__(self):
+        
+        noShards = self.capi.get_number_of_shards()
+        for i in range(0,noShards):
+            self.shard_list.append(CascadeShardLinq(self.capi, self.type, self.subgroup_index, i, self.version))
+        return self
+
+    def __next__(self):
+        while self.ptr < len(self.shard_list):
+            csl = self.shard_list[self.ptr]
+            try:
+                value = csl.__next__()
+                return value.decode('utf-8')
+            except:
+                ptr+=1
+        raise StopIteration
+        
+        
+
